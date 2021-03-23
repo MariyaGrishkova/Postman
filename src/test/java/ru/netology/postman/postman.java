0820407276c1c355811b001f9410103059bfe07e
Mariya Grishkova <galeyevamv@gmail.com>
2021-03-23 20:22:35 +0300

    Initial commit

---
+++ src/test/java/ru/netology/postman/postman.java
@@ -0,0 +1,21 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanApiTest {
+    @Test
+    void shouldReturnData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
